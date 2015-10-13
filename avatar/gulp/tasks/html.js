7ea3d25c93ebb57cd5bae5eeeb7003e17ecf4633
cs <bartus.csongor@gmail.com>
2015-10-13 12:20:30 +0300

    Montserrat added

--- avatar/gulp/tasks/html.js
+++ avatar/gulp/tasks/html.js
@@ -49,7 +49,7 @@ var html = function(source, dest) {
       }
     }))
 
-    .pipe(minifyHTML())
+    //.pipe(minifyHTML())
     .pipe(gulp.dest(dest));
 }
 
--- avatar/gulp/tasks/scss.js
+++ avatar/gulp/tasks/scss.js
@@ -34,7 +34,7 @@ var _scss = function(source, dest) {
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(postcss([ autoprefixer() ]))
-    .pipe(minifyCSS())
+    //.pipe(minifyCSS())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(dest));
 }

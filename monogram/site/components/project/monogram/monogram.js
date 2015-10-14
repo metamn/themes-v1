d6beef75c2f6219c00d20f46dc9683c91be7dda9
cs <bartus.csongor@gmail.com>
2015-10-14 10:54:37 +0300

    Making momongram size adapting to various texts

--- monogram/site/components/project/monogram/monogram.js
+++ monogram/site/components/project/monogram/monogram.js
@@ -6,11 +6,30 @@ var monogram = function(containerID) {
   var chars = text.split("");
   for (var i = 0; i < text.length; i++) {
     var item = document.createElement('div');
-    item.className = 'monogram__character';
+    item.className = 'monogram__character ' + monogramCharacterSize(text.length);
     item.innerHTML = chars[i];
     container.appendChild(item);
   }
 };
+
+
+// Compute a responsive size for each monogram character
+// - returns a `scale` classname
+function monogramCharacterSize(length) {
+  switch (true) {
+    case (length <= 5):
+      return 'text-style--character-s';
+      break;
+    case (length <= 10):
+      return 'text-style--character-m';
+      break;
+    case (length <= 15):
+      return 'text-style--character-l';
+      break;
+    default:
+      return 'text-style--character-xl';
+  }
+}
 
 
 monogram('.monogram');
